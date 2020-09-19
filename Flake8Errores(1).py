45f1be256815d47a61df630997c234eac048145b
murilloAD <70825503+murilloAD@users.noreply.github.com>
2020-09-18 23:52:20 -0600

    Update Flake8Errores(1).py

--- Flake8Errores(1).py
+++ Flake8Errores(1).py
@@ -1,11 +1,7 @@
-import numpy as np
-# F401 'numpy as np' imported but unused
+x = input("Ingrese un número entero para determinar si es par o impar:")
 
-x= input("Ingrese un número entero para determinar si es par o impar:")
-# E225 missing whitespace around operator
 
-if x % 2 == 0:   
-    # W291 trailing whitespace
+if x % 2 == 0:
     print("El número que usted ha ingresado es un número par")
 else:
     print("El número que usted ha ingresado es un número impar")
